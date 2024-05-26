2b425850719e6a4ee52ee89acf0c94abf12738ab
WRC <hello_wrc@outlook.com>
2024-05-26 12:19:14 +0800

    初始化仓库

---
+++ src/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app-bar>
+    <v-app-bar-title>ClassIsland</v-app-bar-title>
+    
+    <template v-slot:append>
+      <v-btn small variant="flat" color="primary">立即下载</v-btn>
+    </template>
+  </v-app-bar>
+  
+  <!-- 主要介绍 -->
+  <div class="">
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script lang="ts" setup>
+
+</script>
