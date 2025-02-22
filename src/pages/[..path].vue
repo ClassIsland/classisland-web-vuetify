3f792e85b76fafae282814e252e727931b972249
WRC <hello_wrc@outlook.com>
2025-02-22 12:01:45 +0800

    fix: 修复 404 页面无法正常显示的问题

---
+++ src/pages/[..path].vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="page-margin-x mt-12">
+    <h2 class="align-self-center text-center mb-6 text-h3 font-weight-bold">Σ(っ °Д °;)っ</h2>
+    <h2 class="align-self-center text-center mb-6 text-h4 font-weight-bold">404</h2>
+    <p class="text-center align-self-center mb-16">找不到请求的页面。</p>
+
+    <div class="justify-center d-flex flex-row flex-wrap">
+      <v-btn color="blue-lighten-3" prepend-icon="mdi-home" to="/">返回首页</v-btn>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
